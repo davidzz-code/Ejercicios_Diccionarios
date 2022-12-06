b5d6621dd0caaaabb6f06b61e8f9c96a2844d3ce
David Ramírez <dramirezruiz@cifpfbmoll.eu>
2022-12-06 21:32:40 +0100

    Finished menu and comments

--- ejercicio-5.py
+++ ejercicio-5.py
@@ -1,11 +1,10 @@
-# Desarrolla un programa, que nos sirva para gestionar nuestros contactos 
-# (la información a gestionar será el teléfono, nombre, apellido1, apellido2 y correo electrónico. 
-# El programa tendrá un menú, con las siguientes opciones: añadir contacto, consultar contacto a partir de la clave, 
-# consultar todos los contactos y eliminar contacto. Aprovecha lo que has aprendido hasta el momento 
-# (diccionarios, funciones, procedimientos…).
+# David Ramírez - AGENDA DE CONTACTOS
 
 # Creamos la lista en el programa principal para que todas las funciones la puedan usar como global
 contactos = []
+
+# Función que añade los contactos a la lista.
+# Cada elemento de la lista es un diccionario distinto 
 def addContact():
     dict = {}
     nombre = input("Dime el nombre: ")
@@ -25,20 +24,34 @@ def consultarTodos():
     return contactos
 
 # Función que consulta contactos a través de su posición en la lista. El índice será el número clave que introduce el usuario
-def consultarKey(clave):
-    return contactos[clave]
+def consultarKey(claveParam):
+    return contactos[claveParam]
 
-def eliminarContactos(posicion):
-    return contactos.pop(posicion)
+# Función que borra el contacto a través de su posición en la lista
+def eliminarContactos(posicionParam):
+    return contactos.pop(posicionParam)
 
 
 
+print("Bienvenido/a a la agenda, ¿que operación quieres realizar?:")
+menu = input("- Añadir contacto \n- Consultar por clave \n- Consultar todos \n- Eliminar contacto \n- Salir \nOpción: ")
 
-# print("¿Que quieres hacer?:")
-# menu = input("- Añadir contacto" \n 
-#             "- Consultar por clave" \n
-###################### claveUser = int(input("Dime el número clave del contacto que quieres consultar: ")) print(consultarKey(claveUser))
-#             "- Consultar por todos" \n
-#             "- Eliminar todos" \n
-#             "- Salir")
-
+# Bucle while que llama a las distintas funciones según la elección del usuario.
+while menu != "Salir":
+    if menu == "Añadir contacto":
+        addContact()
+        print("Añadido con éxito")
+        menu = input("- Añadir contacto \n- Consultar por clave \n- Consultar todos \n- Eliminar contacto \n- Salir \nOpción: ")
+    elif menu == "Consultar por clave":
+        claveVar = int(input("Dime el la posición en la agenda del contacto que quieres consultar: ")) 
+        print(consultarKey(claveVar))
+        menu = input("- Añadir contacto \n- Consultar por clave \n- Consultar todos \n- Eliminar contacto \n- Salir \nOpción: ")
+    elif menu == "Consultar todos":
+        print(f"Estos son todos los contactos: {consultarTodos()}")
+        menu = input("- Añadir contacto \n- Consultar por clave \n- Consultar todos \n- Eliminar contacto \n- Salir \n Opción: ")
+    elif menu == "Eliminar contacto":
+        posicionVar = int(input("Dime el la posición en la agenda del contacto que quieres eliminar: ")) 
+        print(f"Has eliminado a: {eliminarContactos(posicionVar)}")
+        menu = input("- Añadir contacto \n- Consultar por clave \n- Consultar todos \n- Eliminar contacto \n- Salir \nOpción: ")
+    else:
+        menu = input("..:Esa opción no existe, por favor elige una opción:.. : \n- Añadir contacto \n- Consultar por clave \n- Consultar todos \n- Eliminar contacto \n- Salir \nOpción: ")
